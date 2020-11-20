7f1b11f0f650d0b7c9eee78c75d9728267ee686d
David <david2022es@gmail.com>
2020-11-21 00:27:56 +0100

    Post network manager

--- Android/AndroidStudio/app/src/main/java/com/example/serpumar/sprint0_3a/NetworkManager.java
+++ Android/AndroidStudio/app/src/main/java/com/example/serpumar/sprint0_3a/NetworkManager.java
@@ -1,9 +1,17 @@
 package com.example.serpumar.sprint0_3a;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class NetworkManager {
     private static final String TAG = "NetworkManager";
@@ -14,32 +22,67 @@ public class NetworkManager {
     //for Volley API
     public RequestQueue requestQueue;
 
-    private NetworkManager(Context context)
-    {
+    private NetworkManager(Context context) {
         requestQueue = Volley.newRequestQueue(context.getApplicationContext());
         //other stuf if you need
     }
 
-    public static synchronized NetworkManager getInstance(Context context)
-    {
+    public static synchronized NetworkManager getInstance(Context context) {
         if (null == instance)
             instance = new NetworkManager(context);
         return instance;
     }
 
     //this is so you don't need to pass context each time
-    public static synchronized NetworkManager getInstance()
-    {
-        if (null == instance)
-        {
+    public static synchronized NetworkManager getInstance() {
+        if (null == instance) {
             throw new IllegalStateException(NetworkManager.class.getSimpleName() +
                     " is not initialized, call getInstance(...) first");
         }
         return instance;
     }
 
-    public interface ControladorRespuestas<T>
-    {
+    public void postRequest(JSONObject jsonParametros, String stringRequest, final ControladorRespuestas controladorRespuestas) {
+
+        requestQueue.start();
+
+        JsonRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url + stringRequest, jsonParametros, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Response", "Guardado en base de datos");
+                controladorRespuestas.getResult(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error.Response", error.toString());
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    public void putRequest(JSONObject jsonParametros, String stringRequest, final ControladorRespuestas controladorRespuestas) {
+
+        requestQueue.start();
+
+        JsonRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url + stringRequest, jsonParametros, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Response", "Guardado en base de datos");
+                controladorRespuestas.getResult(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error.Response", error.toString());
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    public interface ControladorRespuestas<T> {
         void getResult(T object);
     }
 }
