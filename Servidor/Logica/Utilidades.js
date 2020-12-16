742e8eb47a4b6dccf2335f03d3dd8f0354981de1
David <david2022es@gmail.com>
2020-12-16 23:55:01 +0100

    ObtenerLecturas + Leer interpolación

--- Servidor/Logica/Utilidades.js
+++ Servidor/Logica/Utilidades.js
@@ -6,111 +6,116 @@ const calidadAireMult = {
 }
 
 const calidadAire = {
-  0: "Muy buena",
-  1: "Buena",
-  2: "Media",
-  3: "Mala",
-  4: "Peligroso"
+    0: "Muy buena",
+    1: "Buena",
+    2: "Media",
+    3: "Mala",
+    4: "Peligroso"
 }
 
 module.exports = class Utilidades {
 
-  // .................................................................
-  // JSON(mediciones oficiales) -> convertirAJSONpropio() --> JSON
-  // .................................................................
-  convertirAJSONpropio(csv) {
-    var text = '{"fecha":"' + this.fechaActual() + '", "lecturas":[';
-    var calidad, total;
-    csv.forEach(element => {
-      total = element['SO2 (µg/m³)'] + element['NO2 (µg/m³)'] + element['O3 (µg/m³)'];
-      calidad = this.procesarCalidadAire(element, total)
-      text += '{"contaminación":' + total + ',"hora":' + element['Hora Solar'] + ',"calidad":"' + calidadAire[calidad] + '",'
-        + '"contaminantes":{"SO2 (µg/m³)":' + element['SO2 (µg/m³)'] + ',"NO2 (µg/m³)":' + element['NO2 (µg/m³)'] + ',"O3 (µg/m³)":' + element['O3 (µg/m³)'] + '}}';
-      if (element['Hora Solar'] != '23') text += ','
-      else text += ']}'
-    });
-    return JSON.parse(text)
-  }
-
-  // .................................................................
-  // JSON(mediciones oficiales en tiempo real) -> convertirTiempoRealAJSONpropio() --> JSON
-  // .................................................................
-  convertirTiempoRealAJSONpropio(csv) {
-    var text;
-    var calidad, total, i = 0;
-    var horaAnterior = 0
-    var sumas = []
-    sumas.push(0)
-    var total = 0
-    var lecturas = ""
-    csv.forEach(element => {
-      var abreviatura = element.abreviatura;
-      if (horaAnterior != element.hora && horaAnterior != 0) {
-        i++
-        sumas.push(0)
-      }
-      if (element.abreviatura == "SO2" || element.abreviatura == "NO2" || element.abreviatura == "O3") {
-        var temporal = element.valor;
-        horaAnterior = element.hora
-        sumas[i] += temporal
-      }
-      //lecturas += "{'"+ abreviatura+"':"+ element.valor +"}"
-    });
-    i= 0
-    sumas.forEach(suma =>{
-      lecturas += '{"contaminación":' + suma + ',"calidad":"'+ calidadAire[this.procesarCalidadAire({"tipoMedicion":"O3"},suma)] +'"}'
-      total += suma
-      if(i!=sumas.length-1) lecturas += ","
-      i++
-    })
-    total = total/sumas.length
-    text = '{"fecha":"' + this.fechaActual() + '", "calidadMedia":"'+ calidadAire[this.procesarCalidadAire({"tipoMedicion":"O3"},total)] +'", "mediciones":[' + lecturas + "]}"
-    return JSON.parse(text)
-  }
-
-  // .................................................................
-  // String -> convertirAJSONpropio() --> JSON
-  // .................................................................
-  procesarCalidadAireAJSON(json) {
-    var text = '{"mediciones":['
-    var i = 0, media = 0;
-    var calidadNum
-    json.forEach(element => {
-      calidadNum = this.procesarCalidadAire(element, element['valor'])
-      media += calidadNum;
-      //Construcción del JSON
-      text += '{"valor":' + element['valor'] + ',"momento":"' + element['momento']
-        + '","ubicacion":"' + element['ubicacion'] + '","tipoMedicion":"' + element['tipoMedicion']
-        + '","calidad":"' + calidadAire[calidadNum] + '"}' + (i++ < json.length - 1 ? ',' : '],"calidadMedia":"' + calidadAire[Math.round(media / json.length)] + '"}')
-    })
-    console.log(text + " - " + media + " : " + json.length)
-    return JSON.parse(text);
-  }
-
-  procesarCalidadAire(element, valor) {
-    //Sacamos el indice de contaminación partiendo la medida por el denominador de contaminación
-    var calidadNum = Math.floor(valor / calidadAireMult[element['tipoMedicion']])
-    //Si es Ozono, la calidad del aire va escalada uniformemente, en cambio tiene crecimiento exponencial.
-    if (element['tipoMedicion'] == "O3") {
-      calidadNum = (calidadNum > 4 ? 4 : calidadNum)
-    } else {
-      if (calidadNum >= 8) calidadNum = 4
-      else if (calidadNum >= 4 && calidadNum < 8) calidadNum = 3
-      else if (calidadNum >= 2 && calidadNum < 4) calidadNum = 2
+    // .................................................................
+    // JSON(mediciones oficiales) -> convertirAJSONpropio() --> JSON
+    // .................................................................
+    convertirAJSONpropio(csv) {
+        var text = '{"fecha":"' + this.fechaActual() + '", "lecturas":[';
+        var calidad, total;
+        csv.forEach(element => {
+            total = element['SO2 (µg/m³)'] + element['NO2 (µg/m³)'] + element['O3 (µg/m³)'];
+            calidad = this.procesarCalidadAire(element, total)
+            text += '{"contaminación":' + total + ',"hora":' + element['Hora Solar'] + ',"calidad":"' + calidadAire[calidad] + '",' +
+                '"contaminantes":{"SO2 (µg/m³)":' + element['SO2 (µg/m³)'] + ',"NO2 (µg/m³)":' + element['NO2 (µg/m³)'] + ',"O3 (µg/m³)":' + element['O3 (µg/m³)'] + '}}';
+            if (element['Hora Solar'] != '23') text += ','
+            else text += ']}'
+        });
+        return JSON.parse(text)
     }
-    return calidadNum;
-  }
+
+    // .................................................................
+    // JSON(mediciones oficiales en tiempo real) -> convertirTiempoRealAJSONpropio() --> JSON
+    // .................................................................
+    convertirTiempoRealAJSONpropio(csv) {
+        var text;
+        var calidad, total, i = 0;
+        var horaAnterior = 0
+        var sumas = []
+        sumas.push(0)
+        var total = 0
+        var lecturas = ""
+        csv.forEach(element => {
+            var abreviatura = element.abreviatura;
+            if (horaAnterior != element.hora && horaAnterior != 0) {
+                i++
+                sumas.push(0)
+            }
+            if (element.abreviatura == "SO2" || element.abreviatura == "NO2" || element.abreviatura == "O3") {
+                var temporal = element.valor;
+                horaAnterior = element.hora
+                sumas[i] += temporal
+            }
+            //lecturas += "{'"+ abreviatura+"':"+ element.valor +"}"
+        });
+        i = 0
+        sumas.forEach(suma => {
+            lecturas += '{"contaminación":' + suma + ',"calidad":"' + calidadAire[this.procesarCalidadAire({
+                "tipoMedicion": "O3"
+            }, suma)] + '"}'
+            total += suma
+            if (i != sumas.length - 1) lecturas += ","
+            i++
+        })
+        total = total / sumas.length
+        text = '{"fecha":"' + this.fechaActual() + '", "calidadMedia":"' + calidadAire[this.procesarCalidadAire({
+            "tipoMedicion": "O3"
+        }, total)] + '", "mediciones":[' + lecturas + "]}"
+        return JSON.parse(text)
+    }
+
+    // .................................................................
+    // String -> convertirAJSONpropio() --> JSON
+    // .................................................................
+    procesarCalidadAireAJSON(json) {
+        var text = '{"mediciones":['
+        var i = 0,
+            media = 0;
+        var calidadNum
+        json.forEach(element => {
+            calidadNum = this.procesarCalidadAire(element, element['valor'])
+            media += calidadNum;
+            //Construcción del JSON
+            text += '{"valor":' + element['valor'] + ',"momento":"' + element['momento'] +
+                '","ubicacion":"' + element['ubicacion'] + '","tipoMedicion":"' + element['tipoMedicion'] +
+                '","calidad":"' + calidadAire[calidadNum] + '"}' + (i++ < json.length - 1 ? ',' : '],"calidadMedia":"' + calidadAire[Math.round(media / json.length)] + '"}')
+        })
+        console.log(text + " - " + media + " : " + json.length)
+        return JSON.parse(text);
+    }
+
+    procesarCalidadAire(element, valor) {
+        //Sacamos el indice de contaminación partiendo la medida por el denominador de contaminación
+        var calidadNum = Math.floor(valor / calidadAireMult[element['tipoMedicion']])
+        //Si es Ozono, la calidad del aire va escalada uniformemente, en cambio tiene crecimiento exponencial.
+        if (element['tipoMedicion'] == "O3") {
+            calidadNum = (calidadNum > 4 ? 4 : calidadNum)
+        } else {
+            if (calidadNum >= 8) calidadNum = 4
+            else if (calidadNum >= 4 && calidadNum < 8) calidadNum = 3
+            else if (calidadNum >= 2 && calidadNum < 4) calidadNum = 2
+        }
+        return calidadNum;
+    }
 
 
-  fechaActual() {
-    var fecha = new Date();
-    var dd = String(fecha.getDate()).padStart(2, '0');
-    var mm = String(fecha.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = fecha.getFullYear();
+    fechaActual() {
+        var fecha = new Date();
+        var dd = String(fecha.getDate()).padStart(2, '0');
+        var mm = String(fecha.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = fecha.getFullYear();
 
-    fecha = dd + '/' + mm + '/' + yyyy;
-    return fecha;
-  }
+        fecha = dd + '/' + mm + '/' + yyyy;
+        return fecha;
+    }
 
     // ................................................................................................................................................
     // <R>, <R>, <R>, <R>
@@ -131,6 +136,37 @@ module.exports = class Utilidades {
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
         return R * c;
+    }
+
+    parsearMedicion(mediciones) {
+
+        var string = ""
+
+        for (var i = 0; i < mediciones.length; i++) {
+
+            string += mediciones[i].ubicacion.split(",")[0]
+            if (i < mediciones.length - 1) string += ", "
+        }
+
+        string += "\n"
+
+        for (var i = 0; i < mediciones.length; i++) {
+
+            string += mediciones[i].ubicacion.split(",")[1]
+            if (i < mediciones.length - 1) string += ", "
+        }
+
+        string += "\n"
+
+        for (var i = 0; i < mediciones.length; i++) {
+
+            string += mediciones[i].valor
+            if (i < mediciones.length - 1) string += ", "
+        }
+        
+        string += "\n"
+
+        return string
     }
 
 }
